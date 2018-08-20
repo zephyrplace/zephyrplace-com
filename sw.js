0dab6cfe4da7111180309832105eff748edb62f5
tiago-marques <me@tiagomarques.com.br>
2018-08-20 09:01:27 -0300

    using staleWhileRevalidate Policy

--- sw.js
+++ sw.js
@@ -1,8 +1,32 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
-    workbox.precaching.precacheAndRoute([]);
+    workbox.googleAnalytics.initialize();
+    workbox.core.setCacheNameDetails({
+        prefix: 'zp',
+        suffix: 'v0',
+        precache: 'custom-precache-name',
+        runtime: 'custom-runtime-name'
+    });
+    workbox.routing.registerRoute(
+        /.*\.(?:png|jpg|jpeg|svg|gif)/g,
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'my-image-cache',
+        })
+    );
+    workbox.routing.registerRoute(
+        "/index.html",
+        workbox.strategies.staleWhileRevalidate()
+    );
+    workbox.routing.registerRoute(
+        /.*\.(?:js)/g,
+        workbox.strategies.staleWhileRevalidate()
+    );
+    workbox.routing.registerRoute(
+        /.*\.(?:css)/g,
+        workbox.strategies.staleWhileRevalidate()
+    );
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
