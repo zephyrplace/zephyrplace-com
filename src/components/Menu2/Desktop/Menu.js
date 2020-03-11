f388969b16b4a774ec98a57fc39fcb7f918d23f2
jpdalbozp <joao.dalbo@zephyrplace.com>
2020-03-11 12:06:58 -0300

    fixing routes

--- src/components/Menu2/Desktop/Menu.js
+++ src/components/Menu2/Desktop/Menu.js
@@ -1,28 +1,37 @@
-import React from 'react'
-import { A } from './styles'
+import React from "react";
+import { A } from "./styles";
 
-import logo from '../../../assets/svg/Caminho 12.svg'
-import zephyrplace from '../../../assets/svg/Grupo 572.svg'
+import logo from "../../../assets/svg/Caminho 12.svg";
+import zephyrplace from "../../../assets/svg/Grupo 572.svg";
+import { useHistory } from "react-router-dom";
 
-export default function Desktop () {
+export default function Desktop() {
+  const history = useHistory();
+
   return (
-    <div className='flex justify-between p-4'>
-      <div className='flex justify-around'>
-        <img src={logo} className='flex-1' alt='logo' />
-        <img src={zephyrplace} className='flex-1 ml-10' alt='logo' />
+    <div className="flex justify-between p-4">
+      <div className="flex justify-around">
+        <img src={logo} className="flex-1" alt="logo" />
+        <img src={zephyrplace} className="flex-1 ml-10" alt="logo" />
       </div>
 
-      <div className='flex flex-auto justify-around items-center'>
-        <A href='/' className='text-black text-lg font-bold'>
+      <div className="flex flex-auto justify-around items-center">
+        <A href="/" className="text-black text-lg font-bold">
           HOME
         </A>
-        <A href='/Projects' className='text-black text-lg font-bold'>
+        <A
+          onClick={() => history.push("/Projects")}
+          className="text-black text-lg font-bold"
+        >
           PROJETOS
         </A>
-        <A href='/About' className='text-black text-lg font-bold'>
+        <A
+          onClick={() => history.push("/About")}
+          className="text-black text-lg font-bold"
+        >
           SOBRE NÓS
         </A>
       </div>
     </div>
-  )
+  );
 }
