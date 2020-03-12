e501b15cf28664b1c22f25fe6e904c013bcdf970
jpdalbozp <joao.dalbo@zephyrplace.com>
2020-03-12 11:38:04 -0300

    Updates

---
+++ precache-manifest.ac2f336d3d3a49d5fcd19dd35af02785.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2460039552eee2f28858639589a263cc",
+    "url": "/Home/index.html"
+  },
+  {
+    "revision": "fd529921b2b30379203f",
+    "url": "/Home/static/css/2.9c680c11.chunk.css"
+  },
+  {
+    "revision": "a7fb5163e442077e6888",
+    "url": "/Home/static/css/main.76aac0e5.chunk.css"
+  },
+  {
+    "revision": "fd529921b2b30379203f",
+    "url": "/Home/static/js/2.9840d5ff.chunk.js"
+  },
+  {
+    "revision": "c9d490cefc26f07be1cfc2b04269560c",
+    "url": "/Home/static/js/2.9840d5ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7fb5163e442077e6888",
+    "url": "/Home/static/js/main.491d127f.chunk.js"
+  },
+  {
+    "revision": "6d5d66849112ddfa9137",
+    "url": "/Home/static/js/runtime-main.b8667713.js"
+  },
+  {
+    "revision": "a1bfa66abf36351c1426605908bedf85",
+    "url": "/Home/static/media/Apoyo.a1bfa66a.jpg"
+  },
+  {
+    "revision": "a251e9868f318500a837e43d9d0edef6",
+    "url": "/Home/static/media/Caminho 1.a251e986.svg"
+  },
+  {
+    "revision": "4c84fe6386a1e72c8443e3e4d843b1ba",
+    "url": "/Home/static/media/CaraCaminho.4c84fe63.svg"
+  },
+  {
+    "revision": "811357dda845c8236e8ef323ac8f55b0",
+    "url": "/Home/static/media/Equipe1.811357dd.jpg"
+  },
+  {
+    "revision": "fce7ca57e74a74bf1085046b5577326e",
+    "url": "/Home/static/media/Equipe2.fce7ca57.jpg"
+  },
+  {
+    "revision": "fa677b5e880edf72e2ab7f814cba35a7",
+    "url": "/Home/static/media/Equipe3.fa677b5e.jpg"
+  },
+  {
+    "revision": "c193c3023d3f668e63e987578915c805",
+    "url": "/Home/static/media/Equipe4.c193c302.jpg"
+  },
+  {
+    "revision": "68a2f3a1a924df0f41c75290f0d0b359",
+    "url": "/Home/static/media/Equipe5.68a2f3a1.jpg"
+  },
+  {
+    "revision": "fe2603682cab4f866e5e3f27eafb4b38",
+    "url": "/Home/static/media/Equipe6.fe260368.jpg"
+  },
+  {
+    "revision": "8c373787abd547d3ae7a7ba3be3503cd",
+    "url": "/Home/static/media/Fogete.8c373787.png"
+  },
+  {
+    "revision": "a3d101bb5ac02b3ee7baf9386d4bc24a",
+    "url": "/Home/static/media/Grupo 57.a3d101bb.svg"
+  },
+  {
+    "revision": "5b20176d7158c9e892b83d0b99ce74fc",
+    "url": "/Home/static/media/Grupo.5b20176d.jpg"
+  },
+  {
+    "revision": "4aebf2d21d11b9069f0ae7d33dc8dbdb",
+    "url": "/Home/static/media/JWF.4aebf2d2.jpg"
+  },
+  {
+    "revision": "1b28eb82af1f2815e1cdbe3026ec2f51",
+    "url": "/Home/static/media/LOGOBRANCOSVG.1b28eb82.svg"
+  },
+  {
+    "revision": "d47c3b4d752ff47921a96eb1f7897502",
+    "url": "/Home/static/media/LinkedIn.d47c3b4d.svg"
+  },
+  {
+    "revision": "b8f5e5013a94794427b07980366c2919",
+    "url": "/Home/static/media/MockupBPP.b8f5e501.gif"
+  },
+  {
+    "revision": "a2da0dd679ca12d67dbccb8e49de1854",
+    "url": "/Home/static/media/MockupCAP.a2da0dd6.gif"
+  },
+  {
+    "revision": "a1cb8be05eb417bf86532b5aeb6a7116",
+    "url": "/Home/static/media/MockupTVM.a1cb8be0.gif"
+  },
+  {
+    "revision": "637ae4091f997ddb612e7623cc2c8724",
+    "url": "/Home/static/media/Mulher-lampada.637ae409.svg"
+  },
+  {
+    "revision": "cde47fe0329649ea2baee5ed63f28e02",
+    "url": "/Home/static/media/Mulher.cde47fe0.svg"
+  },
+  {
+    "revision": "2ff34154889f421a387924abe50ec87a",
+    "url": "/Home/static/media/Section1.2ff34154.svg"
+  },
+  {
+    "revision": "4af3eacce6ac66613afcad69dae9afe2",
+    "url": "/Home/static/media/Section2.4af3eacc.svg"
+  },
+  {
+    "revision": "c1e8e81527b2ec96b0395ae8cd33aa90",
+    "url": "/Home/static/media/Section3.c1e8e815.svg"
+  },
+  {
+    "revision": "b9eecacf04be28d33281db7c684f15e8",
+    "url": "/Home/static/media/Section4.b9eecacf.svg"
+  },
+  {
+    "revision": "336f16cca80f092e8ebe4c32b5405e81",
+    "url": "/Home/static/media/Section5.336f16cc.svg"
+  },
+  {
+    "revision": "f8ba8845d1a0fa72253edc41e02e4f2d",
+    "url": "/Home/static/media/Section6.f8ba8845.svg"
+  },
+  {
+    "revision": "7ec0a17b6a699a87b87dc1264131add9",
+    "url": "/Home/static/media/arrowdown.7ec0a17b.svg"
+  },
+  {
+    "revision": "4efbb7f040616a0c89e6598bc0976e92",
+    "url": "/Home/static/media/atomobg.4efbb7f0.svg"
+  },
+  {
+    "revision": "55acc85a3b11c298b22419f51e4a4d94",
+    "url": "/Home/static/media/balao_git.55acc85a.png"
+  },
+  {
+    "revision": "4cfee7bc388a9559220c02db1a6c85af",
+    "url": "/Home/static/media/balao_illustrator.4cfee7bc.png"
+  },
+  {
+    "revision": "e65d0b2d11a97a020d5a2563871ccfb8",
+    "url": "/Home/static/media/balao_node.e65d0b2d.png"
+  },
+  {
+    "revision": "700a51ab40e8199b18226f7d7dbf6d91",
+    "url": "/Home/static/media/balao_react.700a51ab.png"
+  },
+  {
+    "revision": "65bc118288169af3a3eee4d5c1c3b268",
+    "url": "/Home/static/media/balao_xd.65bc1182.png"
+  },
+  {
+    "revision": "361456cf4288258ba8f97fdbe686e9b4",
+    "url": "/Home/static/media/carasentado.361456cf.svg"
+  },
+  {
+    "revision": "69cd6cdc3d1be02efe855e3311628ac6",
+    "url": "/Home/static/media/facebook.69cd6cdc.svg"
+  },
+  {
+    "revision": "7b0c3f57fd006f0f6ccbbef94ae2fa55",
+    "url": "/Home/static/media/fogetevoando.7b0c3f57.svg"
+  },
+  {
+    "revision": "342266a828331196d7056490bba7b6fb",
+    "url": "/Home/static/media/fogetinhobg.342266a8.svg"
+  },
+  {
+    "revision": "505bd1147cfe799b8fffc50f5fcc0866",
+    "url": "/Home/static/media/github.505bd114.svg"
+  },
+  {
+    "revision": "3620848f3668f961bdc490616d90fb70",
+    "url": "/Home/static/media/img_projetos.3620848f.png"
+  },
+  {
+    "revision": "323603f257fa71002242d055d1e50b99",
+    "url": "/Home/static/media/img_sobre.323603f2.png"
+  },
+  {
+    "revision": "0646add9329b410d1522c3d228fcd0c3",
+    "url": "/Home/static/media/instagram.0646add9.svg"
+  },
+  {
+    "revision": "1614b483303e7a7b77abf65b10ee9e2e",
+    "url": "/Home/static/media/quadrados.1614b483.svg"
+  },
+  {
+    "revision": "b87da1360bb7f36206c14f64fa4cb50d",
+    "url": "/Home/static/media/telescopiobg.b87da136.svg"
+  },
+  {
+    "revision": "9c53ee6778a8a1751999cf6f27120759",
+    "url": "/Home/static/media/whatsapp.9c53ee67.svg"
+  }
+]);
